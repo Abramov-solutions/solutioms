2e785df0aff7016af25f86cf02afd583a1f72bab
Abramov-solutions <62617551+Abramov-solutions@users.noreply.github.com>
2020-03-24 21:51:50 +0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double r1, r2, r3, r;
+    cin >> r1 >> r2 >> r3;
+    r = 1 / r1 + 1 / r2 + 1/ r3;
+    r = 1 / r;
+    cout << r;
+}
